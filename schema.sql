67c61c9b137d7df3d943300e4780d6680642fa79
jmcuya1305 <juliannemargarettecuya@outlook.com>
2025-11-06 14:45:49 +0800

    CREATED A TABLE TEACHERS

--- schema.sql
+++ schema.sql
@@ -1,2 +1,4 @@
 CREATED DATABASE school_db;
 USE school_db;CREATE TABLE student (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), grade INT);
+INSERT INTO teachers(id, name, subject) VALUES (1, 'MARIELLE', 'DBMS');
+INSERT INTO teachers(id, name, subject) VALUES (1, Marielle, DBMS);
